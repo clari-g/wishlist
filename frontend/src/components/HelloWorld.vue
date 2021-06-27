23a15e53e7282bb89a07a063f615d4da20b97f97
Clari Garcia <clari.xiv@gmail.com>
2021-06-27 15:53:11 -0300

    Bootstrap, edit item

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>Wishlist</h1>
     <p>
       lorem ipsum dolor sit    
@@ -7,13 +7,39 @@
 
     <h3>Ingresar URL</h3>
 
-    <input v-model="url" v-on:keyup.enter="getInfo">
-    <button v-on:click="getInfo">+</button>
+    <div class="inputArea">
+      <b-input-group>
+        <b-form-input v-show="locked" v-model="url" v-on:keyup.enter="unlock"></b-form-input>
+        <b-form-input v-show="!locked" v-model="url" v-on:keyup.enter="getInfo"></b-form-input>
+
+        <b-input-group-append>
+          <b-button v-show="locked" v-on:click="unlock">
+            <b-icon icon="lock" />
+          </b-button>
+          <b-button v-show="!locked" v-on:click="getInfo">
+            <b-icon icon="plus" />
+          </b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </div>
 
     <div class="itemList">
-      <div v-for="(item, index) in items" :key="item.id">
-        {{ item.title }} <button v-on:click="deleteItem(index)">-</button>
-      </div>
+      <b-card v-for="(item, index) in items" :key="index" :img-src="item.image" img-alt="Card image" img-left class="mb-3"
+      style="max-width: 50%;">
+        <b-card-text>
+          <div class="mb-3" v-show="!item.edit">{{ item.title }}</div>
+          <b-form-input class="mb-3" v-show="item.edit" v-model="item.title"></b-form-input>
+          <div>
+            <b-button v-on:click="deleteItem(index)">
+              <b-icon icon="trash" />
+            </b-button>
+            <b-button v-on:click="editItem(index)">
+              <b-icon icon="pencil" />
+            </b-button>
+            <b-button v-on:click="openLink(item.url)">Comprar</b-button>
+          </div>           
+        </b-card-text>
+      </b-card>
     </div>
   </div>
 </template>
@@ -22,6 +48,7 @@
 export default {
   name: 'HelloWorld',
   data: () => ({
+          locked: true,
           url: "",
           items: []
         }),
@@ -41,9 +68,10 @@ export default {
           this.url = "",
           this.items.push({
             favicon: data.favicons[0],
-            images: data.images[0],
+            image: data.images[0],
             title: data.title,
-            url: data.url
+            url: data.url,
+            edit: false
           })
         })       
         .catch(error => {
@@ -52,8 +80,22 @@ export default {
       }
     },
 
+    unlock: function () {
+      if (this.url == "1234") {
+        this.locked = false;
+      }
+    },
+
+    editItem: function (id) {
+      this.items[id].edit = !this.items[id].edit;
+    },
+
     deleteItem: function (id) {
       this.items.splice(id, 1);
+    },
+
+    openLink: function (url) {
+      window.open(url, "_blank");
     }
   }
 
@@ -67,6 +109,10 @@ h3 {
 }
 a {
   color: #42b983;
+}
+.inputArea {
+  width: 50%;
+  margin-left: 25%;
 }
 .itemList {
   margin-top:20px;
